cb3e5495b645baef28c3bcfd55b6609835b43b02
FarrukhAbbasi <144101527+FarrukhAbbasi021@users.noreply.github.com>
2024-06-12 05:16:17 -0700

    Update streamlit_app_py.py

--- streamlit_app_py.py
+++ streamlit_app_py.py
@@ -864,28 +864,4 @@ try:
                         accordion_height = st.session_state["accordion_height"][i]
 
                         
-                        def handle_error(e):
-                             error_message = ''
-                             st.error('An error has occurred. Please try again.', icon="🚨")
-
-                             # Determine the error message
-                             if hasattr(e, 'message'):
-                                 error_message = e.message
-                             else:
-                                 error_message = str(e)
-
-                             st.error('ERROR MESSAGE: {}'.format(error_message), icon="🚨")
-
-                             # Get exception details
-                             exc_type, exc_obj, exc_tb = sys.exc_info()
-                             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                             st.error(f'Error Type: {exc_type}', icon="🚨")
-                             st.error(f'File Name: {fname}', icon="🚨")
-                             st.error(f'Line Number: {exc_tb.tb_lineno}', icon="🚨")
-                        try:
-                            # Your code that may raise an exception goes here
-                            if accordion_height > 0:
-                                st.components.v1.html(accordion_html_code, height=accordion_height)
-
-                        except Exception as e:
-                            handle_error(e)
+                        
