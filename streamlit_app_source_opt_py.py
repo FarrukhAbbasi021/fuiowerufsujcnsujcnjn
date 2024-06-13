476c096514ddffc843162844765ccf4aa5fb26dd
FarrukhAbbasi <144101527+FarrukhAbbasi021@users.noreply.github.com>
2024-06-13 03:02:54 -0700

    Update streamlit_app_source_opt_py.py

--- streamlit_app_source_opt_py.py
+++ streamlit_app_source_opt_py.py
@@ -29,7 +29,7 @@ openai.organization = openai_organization
 openai.api_key = openai_api_key
 
 # Set API key for Pinecone
-pinecone_api_key = "466eb1c0-bd8e-4d70-a299-825a796933c0"
+pinecone_api_key = "3986634d-212b-4aa6-9ff2-4e774220e38e"
 
 # Set API key and environment for Pinecone
 pinecone.set_api_key(pinecone_api_key)
